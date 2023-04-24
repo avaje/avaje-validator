7757aa15939f65aba629088643b0ac1b5082f2ee
Josiah Noel <32279667+SentryMan@users.noreply.github.com>
2023-04-24 10:26:32 -0400

    remove core validation collection

--- validator/src/main/java/io/avaje/validation/adapter/CoreValidation.java
+++ validator/src/main/java/io/avaje/validation/adapter/CoreValidation.java
@@ -1,11 +1,7 @@
 package io.avaje.validation.adapter;
 
-import java.util.Collection;
-
 public interface CoreValidation {
 
     /** Return true to continue validation on this value if needed */
     boolean required(Object value, ValidationRequest ctx, String propertyName);
-
-    boolean collection(ValidationRequest ctx, String propertyName, Collection<?> collection, int minSize, int maxSize);
 }
--- validator/src/main/java/io/avaje/validation/core/DCoreValidation.java
+++ validator/src/main/java/io/avaje/validation/core/DCoreValidation.java
@@ -3,8 +3,6 @@ package io.avaje.validation.core;
 import io.avaje.validation.adapter.CoreValidation;
 import io.avaje.validation.adapter.ValidationRequest;
 
-import java.util.Collection;
-
 final class DCoreValidation implements CoreValidation {
 
     @Override
@@ -14,24 +12,5 @@ final class DCoreValidation implements CoreValidation {
             return false;
         }
         return true;
-    }
-
-    @Override
-    public boolean collection(ValidationRequest ctx, String propertyName, Collection<?> collection, int minSize, int maxSize) {
-        if (collection == null) {
-            if (minSize != -1) {
-                ctx.addViolation("CollectionNull", propertyName);
-            }
-            return false;
-        }
-        final int size = collection.size();
-        if (size < minSize) {
-            ctx.addViolation("CollectionMinSize", propertyName);
-        }
-        if (maxSize > 0 && size > maxSize) {
-            ctx.addViolation("CollectionMaxSize", propertyName);
-        }
-        // also validate the collection elements
-        return size > 0;
     }
 }
