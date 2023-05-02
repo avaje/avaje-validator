e9f3c77643adc7b903b399c9962cede14f414846
Rob Bygrave <robin.bygrave@gmail.com>
2023-05-02 22:18:55 +1200

    Add some javadoc to AdapterContext

--- validator/src/main/java/io/avaje/validation/adapter/AdapterContext.java
+++ validator/src/main/java/io/avaje/validation/adapter/AdapterContext.java
@@ -6,16 +6,30 @@ import java.util.Map;
 
 public interface AdapterContext {
 
+    /**
+     * Return the adapter for the given type.
+     */
     <T> ValidationAdapter<T> adapter(Class<T> cls);
 
+    /**
+     * Return the adapter for the given type.
+     */
     <T> ValidationAdapter<T> adapter(Type type);
 
+    /**
+     * Return the adapter for the given annotation with attributes.
+     */
     <T> ValidationAdapter<T> adapter(Class<? extends Annotation> cls, Map<String, Object> attributes);
 
+    /**
+     * Return the message to use given the global key and attributes.
+     * @param key Used to lookup the fallback default message format to use
+     * @param attributes Attributes that could contain user defined message to use
+     */
     String message(String key, Map<String, Object> attributes);
 
     /**
-     * Factory for creating a ValidationAdapter.
+     * Factory for creating a ValidationAdapter for a given type.
      */
     interface AdapterFactory {
 
@@ -28,7 +42,7 @@ public interface AdapterContext {
     }
 
     /**
-     * Factory for creating a ValidationAdapter.
+     * Factory for creating a ValidationAdapter for a given annotation.
      */
     interface AnnotationFactory {
 
