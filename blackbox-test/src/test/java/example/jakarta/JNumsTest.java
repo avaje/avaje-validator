be23e8df26c912d34be8f34ad76793fb58ba6080
Rob Bygrave <robin.bygrave@gmail.com>
2023-05-22 17:17:41 +1200

    Support for Digits, Positive, Negative, Min, Max

---
+++ blackbox-test/src/test/java/example/jakarta/JNumsTest.java
@@ -0,0 +1,118 @@
+package example.jakarta;
+
+import io.avaje.validation.ConstraintViolation;
+import io.avaje.validation.ConstraintViolationException;
+import io.avaje.validation.Validator;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.Locale;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.fail;
+
+class JNumsTest {
+
+  final Validator validator = Validator.builder().build();
+
+  @Test
+  void valid() {
+    validator.validate(new JNums());
+  }
+
+  @Test
+  void digits() {
+    var bean = new JNums();
+    bean.digits = "12345678";
+    var violation = one(bean);
+    assertThat(violation.message()).isEqualTo("numeric value out of bounds (<5 digits>.<3 digits> expected)");
+  }
+
+  @Test
+  void digitsDE() {
+    var bean = new JNums();
+    bean.digits = "12345678";
+    var violation = one(bean, Locale.GERMAN);
+    assertThat(violation.message()).isEqualTo("numerischer Wert außerhalb des gültigen Bereichs (<5 digits>.<3 digits> erwartet)");
+  }
+
+  @Test
+  void positive() {
+    var bean = new JNums();
+    bean.positive = -1;
+    var violation = one(bean);
+    assertThat(violation.message()).isEqualTo("must be greater than 0");
+  }
+
+  @Test
+  void positiveDE() {
+    var bean = new JNums();
+    bean.positive = -1;
+    var violation = one(bean, Locale.GERMAN);
+    assertThat(violation.message()).isEqualTo("muss größer als 0 sein");
+  }
+
+  @Test
+  void positiveOrZero() {
+    var bean = new JNums();
+    bean.positiveOrZero = -1;
+    var violation = one(bean);
+    assertThat(violation.message()).isEqualTo("must be greater than or equal to 0");
+  }
+
+  @Test
+  void positiveOrZeroDE() {
+    var bean = new JNums();
+    bean.positiveOrZero = -1;
+    var violation = one(bean, Locale.GERMAN);
+    assertThat(violation.message()).isEqualTo("muss größer-gleich 0 sein");
+  }
+
+  @Test
+  void negative() {
+    var bean = new JNums();
+    bean.negative = 1;
+    var violation = one(bean);
+    assertThat(violation.message()).isEqualTo("must be less than 0");
+  }
+
+  @Test
+  void negativeDE() {
+    var bean = new JNums();
+    bean.negative = 1;
+    var violation = one(bean, Locale.GERMAN);
+    assertThat(violation.message()).isEqualTo("muss kleiner als 0 sein");
+  }
+
+  @Test
+  void negativeOrZero() {
+    var bean = new JNums();
+    bean.negativeOrZero = 1;
+    var violation = one(bean);
+    assertThat(violation.message()).isEqualTo("must be less than or equal to 0");
+  }
+
+  @Test
+  void negativeOrZeroDE() {
+    var bean = new JNums();
+    bean.negativeOrZero = 1;
+    var violation = one(bean, Locale.GERMAN);
+    assertThat(violation.message()).isEqualTo("muss kleiner-gleich 0 sein");
+  }
+
+  ConstraintViolation one(Object any) {
+    return one(any, Locale.ENGLISH);
+  }
+
+  ConstraintViolation one(Object any, Locale locale) {
+    try {
+      validator.validate(any, locale);
+      fail("not expected");
+      return null;
+    } catch (ConstraintViolationException e) {
+      var violations = new ArrayList<>(e.violations());
+      assertThat(violations).hasSize(1);
+      return violations.get(0);
+    }
+  }
+}
