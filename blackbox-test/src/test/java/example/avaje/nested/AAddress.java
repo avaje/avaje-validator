74c9f84d99529427930dfb3163ee3d8c90441fea
Rob Bygrave <robin.bygrave@gmail.com>
2023-05-22 22:56:06 +1200

    Add test for nested validation with NotNull ToOne relationship

---
+++ blackbox-test/src/test/java/example/avaje/nested/AAddress.java
@@ -0,0 +1,23 @@
+package example.avaje.nested;
+
+import io.avaje.validation.ValidPojo;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.Size;
+
+@ValidPojo
+public class AAddress {
+
+  @NotBlank
+  public final String line1;
+  @Size(max = 4)
+  public final String line2;
+  @Positive
+  public final long longValue;
+
+  public AAddress(String line1, String line2, long longValue) {
+    this.line1 = line1;
+    this.line2 = line2;
+    this.longValue = longValue;
+  }
+}
