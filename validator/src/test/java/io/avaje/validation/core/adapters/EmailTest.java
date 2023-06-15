b10d98231b4c1dba3f365970e986a3cf3759b332
Josiah Noel <32279667+SentryMan@users.noreply.github.com>
2023-06-15 18:32:42 -0400

    Create EmailTest.java

---
+++ validator/src/test/java/io/avaje/validation/core/adapters/EmailTest.java
@@ -0,0 +1,41 @@
+package io.avaje.validation.core.adapters;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.Map;
+
+import org.junit.jupiter.api.Test;
+
+import io.avaje.validation.adapter.ValidationAdapter;
+import io.avaje.validation.adapter.ValidationContext;
+import io.avaje.validation.core.BasicTest;
+
+class EmailTest extends BasicTest {
+
+  static final ValidationContext ctx = (ValidationContext) validator;
+
+  @interface Email {}
+
+  ValidationAdapter<Object> notBlankAdapter = ctx.adapter(Email.class, Map.of("message", "email"));
+
+  @Test
+  void testNull() {
+    assertThat(notBlankAdapter.validate(null, request)).isTrue();
+  }
+
+  @Test
+  void testValid() {
+    assertThat(notBlankAdapter.validate("someEmail@gmail.com", request)).isTrue();
+  }
+
+  @Test
+  void testBlank() {
+    assertThat(notBlankAdapter.validate("", request)).isTrue();
+    assertThat(notBlankAdapter.validate("                    ", request)).isFalse();
+  }
+
+  @Test
+  void testInvalid() {
+    assertThat(notBlankAdapter.validate("notAnEmail", request)).isFalse();
+  }
+}
