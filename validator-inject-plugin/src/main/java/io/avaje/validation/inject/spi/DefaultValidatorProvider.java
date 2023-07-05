3d1a68f671313709c700e731e889891b43c7853f
Josiah Noel <32279667+SentryMan@users.noreply.github.com>
2023-07-04 23:51:25 -0400

    proper plugin

--- validator-inject-plugin/src/main/java/io/avaje/validation/inject/spi/DefaultValidatorProvider.java
+++ validator-inject-plugin/src/main/java/io/avaje/validation/inject/spi/DefaultValidatorProvider.java
@@ -1,5 +1,8 @@
 package io.avaje.validation.inject.spi;
 
+import java.util.Arrays;
+import java.util.Locale;
+
 import io.avaje.inject.BeanScopeBuilder;
 import io.avaje.validation.Validator;
 
@@ -13,13 +16,31 @@ public final class DefaultValidatorProvider implements io.avaje.inject.spi.Plugi
 
   @Override
   public void apply(BeanScopeBuilder builder) {
+
     builder.provideDefault(
         null,
         Validator.class,
         () -> {
           final var props = builder.propertyPlugin();
+          final var validator =
+              Validator.builder().failFast(props.equalTo("validation.failFast", "true"));
 
-          return Validator.builder().build();
+          props
+              .get("validation.resourcebundle.names")
+              .map(s -> s.split(","))
+              .ifPresent(validator::addResourceBundles);
+
+          props
+              .get("validation.locale.default")
+              .map(Locale::forLanguageTag)
+              .ifPresent(validator::setDefaultLocale);
+
+          props.get("validation.locale.addedLocales").stream()
+              .flatMap(s -> Arrays.stream(s.split(",")))
+              .map(Locale::forLanguageTag)
+              .forEach(validator::addLocales);
+
+          return validator.build();
         });
   }
 }
