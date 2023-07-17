84d5a36cc6da79ee75e42863919b9dce7e997403
Josiah Noel <32279667+SentryMan@users.noreply.github.com>
2023-07-17 18:53:41 -0400

    Update MessageInterpolator.java

--- validator/src/main/java/io/avaje/validation/spi/MessageInterpolator.java
+++ validator/src/main/java/io/avaje/validation/spi/MessageInterpolator.java
@@ -8,9 +8,9 @@ public interface MessageInterpolator {
   /**
    * Interpolate the given message with the annotation attributes
    *
-   * @param template template
-   * @param attributes
-   * @return the interpolated validation error message
+   * @param template The template loaded from annotation/resourceBundle
+   * @param attributes The Constraint annotation's attributes
+   * @return The interpolated validation error message
    */
   String interpolate(String template, Map<String, Object> attributes);
 }
