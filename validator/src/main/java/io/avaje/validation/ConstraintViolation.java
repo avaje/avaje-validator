8db1ef90f91036a2927b2e532004174f77af7cc3
Rob Bygrave <robin.bygrave@gmail.com>
2023-07-27 12:48:40 +1200

    Change ConstraintViolation into a record type

--- validator/src/main/java/io/avaje/validation/ConstraintViolation.java
+++ validator/src/main/java/io/avaje/validation/ConstraintViolation.java
@@ -4,38 +4,6 @@ package io.avaje.validation;
  * Describes a constraint violation. This object exposes the constraint violation context as well as
  * the message describing the violation.
  */
-public class ConstraintViolation {
+public record ConstraintViolation(String path, String propertyName, String message) {
 
-  private final String path;
-  private final String propertyName;
-  private final String message;
-
-  /**
-   * Create the ConstraintViolation with the given path, property and message.
-   */
-  public ConstraintViolation(String path, String propertyName, String message) {
-    this.path = path;
-    this.propertyName = propertyName;
-    this.message = message;
-  }
-
-  /** Return the path for this constraint violation */
-  public String path() {
-    return path;
-  }
-
-  /** Return the property name for this constraint violation */
-  public String propertyName() {
-    return propertyName;
-  }
-
-  /** Return the interpolated error message for this constraint violation */
-  public String message() {
-    return message;
-  }
-
-  @Override
-  public String toString() {
-    return message;
-  }
 }
