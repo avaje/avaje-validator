f4565fa97e119337b4bd78d69ea63d62a4b26d51
Josiah Noel <32279667+SentryMan@users.noreply.github.com>
2023-08-25 20:17:31 -0400

    validate module-info

--- validator-generator/src/main/java/io/avaje/validation/generator/ProcessingContext.java
+++ validator-generator/src/main/java/io/avaje/validation/generator/ProcessingContext.java
@@ -1,13 +1,20 @@
 package io.avaje.validation.generator;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URI;
+import java.util.Collection;
 import java.util.Optional;
+import java.util.Set;
 import java.util.stream.Stream;
 
 import javax.annotation.processing.Filer;
 import javax.annotation.processing.Messager;
 import javax.annotation.processing.ProcessingEnvironment;
+import javax.annotation.processing.RoundEnvironment;
 import javax.lang.model.element.Element;
+import javax.lang.model.element.ModuleElement;
 import javax.lang.model.element.TypeElement;
 import javax.lang.model.type.TypeMirror;
 import javax.lang.model.util.Elements;
@@ -29,6 +36,8 @@ final class ProcessingContext {
     private final Types types;
     private final int jdkVersion;
     private final String diAnnotation;
+    private ModuleElement module;
+    private boolean validated;
 
     Ctx(ProcessingEnvironment env) {
       this.env = env;
@@ -115,6 +124,56 @@ final class ProcessingContext {
 
   static String diAnnotation() {
     return CTX.get().diAnnotation;
+  }
+
+  static void findModule(Set<? extends TypeElement> annotations, RoundEnvironment roundEnv) {
+
+    if (CTX.get().module == null) {
+      CTX.get().module =
+          annotations.stream()
+              .map(roundEnv::getElementsAnnotatedWith)
+              .flatMap(Collection::stream)
+              .findAny()
+              .map(ProcessingContext::getModuleElement)
+              .orElse(null);
+    }
+  }
+
+  static void validateModule(String fqn) {
+    var module = CTX.get().module;
+    if (module != null && !CTX.get().validated && !module.isUnnamed()) {
+
+      CTX.get().validated = true;
+      try {
+        var resource =
+            CTX.get()
+                .filer
+                .getResource(StandardLocation.SOURCE_PATH, "", "module-info.java")
+                .toUri()
+                .toString();
+        try (var inputStream = new URI(resource).toURL().openStream();
+            var reader = new BufferedReader(new InputStreamReader(inputStream))) {
+
+          var noProvides = reader.lines().noneMatch(s -> s.contains(fqn));
+
+          if (noProvides) {
+            logError(
+                module,
+                "Missing \"provides io.avaje.validation.Validator.GeneratedComponent with %s\"",
+                fqn);
+          }
+        }
+      } catch (Exception e) {
+        // can't read module
+      }
+    }
+  }
+
+  static ModuleElement getModuleElement(Element e) {
+    if (e == null || e instanceof ModuleElement) {
+      return (ModuleElement) e;
+    }
+    return getModuleElement(e.getEnclosingElement());
   }
 
   static void clear() {
