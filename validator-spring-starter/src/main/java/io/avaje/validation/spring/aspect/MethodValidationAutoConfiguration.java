e1e58d0df29026c67f271b695dbee24ffa9e97c2
Josiah Noel <32279667+SentryMan@users.noreply.github.com>
2024-05-25 21:27:12 -0400

    working

--- validator-spring-starter/src/main/java/io/avaje/validation/spring/aspect/MethodValidationAutoConfiguration.java
+++ validator-spring-starter/src/main/java/io/avaje/validation/spring/aspect/MethodValidationAutoConfiguration.java
@@ -3,6 +3,7 @@ package io.avaje.validation.spring.aspect;
 import java.util.List;
 
 import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
@@ -11,10 +12,10 @@ import io.avaje.validation.adapter.MethodAdapterProvider;
 
 @Configuration
 @EnableAspectJAutoProxy
-@ConditionalOnBean({Validator.class, MethodAdapterProvider.class})
 public class MethodValidationAutoConfiguration {
 
-  public SpringAOPMethodValidator validator(
+  @Bean
+  public SpringAOPMethodValidator methodValidator(
       Validator validator, List<MethodAdapterProvider> providers) throws Exception {
     return new SpringAOPMethodValidator(validator, providers);
   }
