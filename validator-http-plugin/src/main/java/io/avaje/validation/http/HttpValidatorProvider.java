8c44e1f98133baa8a5db91aec4b233cb6eb74cfc
Rob Bygrave <robin.bygrave@gmail.com>
2024-05-30 19:19:13 +1200

    Format change to HttpValidatorProvider

--- validator-http-plugin/src/main/java/io/avaje/validation/http/HttpValidatorProvider.java
+++ validator-http-plugin/src/main/java/io/avaje/validation/http/HttpValidatorProvider.java
@@ -1,17 +1,19 @@
 package io.avaje.validation.http;
 
+import io.avaje.inject.BeanScopeBuilder;
+
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Locale;
 
-import io.avaje.inject.BeanScopeBuilder;
-
-/** Plugin for avaje inject that provides a default Http Validator instance. */
+/**
+ * Plugin for avaje inject that provides a default Http Validator instance.
+ */
 public final class HttpValidatorProvider implements io.avaje.inject.spi.Plugin {
 
-  private static final Class<?> VALIDATOR_HTTP_CLASS = httpOnCp();
+  private static final Class<?> VALIDATOR_HTTP_CLASS = avajeHttpOnClasspath();
 
-  private static Class<?> httpOnCp() {
+  private static Class<?> avajeHttpOnClasspath() {
     try {
       return Class.forName("io.avaje.http.api.Validator");
     } catch (ClassNotFoundException e) {
@@ -21,7 +23,7 @@ public final class HttpValidatorProvider implements io.avaje.inject.spi.Plugin {
 
   @Override
   public Class<?>[] provides() {
-    return VALIDATOR_HTTP_CLASS == null ? new Class<?>[] {} : new Class<?>[] {VALIDATOR_HTTP_CLASS};
+    return VALIDATOR_HTTP_CLASS == null ? new Class<?>[]{} : new Class<?>[]{VALIDATOR_HTTP_CLASS};
   }
 
   @Override
@@ -30,29 +32,22 @@ public final class HttpValidatorProvider implements io.avaje.inject.spi.Plugin {
       return;
     }
 
-    builder.provideDefault(
-        null,
-        VALIDATOR_HTTP_CLASS,
-        () -> {
-          final var props = builder.propertyPlugin();
+    builder.provideDefault(null, VALIDATOR_HTTP_CLASS, () -> {
+      final var props = builder.propertyPlugin();
+      final var locales = new ArrayList<Locale>();
 
-          final var locales = new ArrayList<Locale>();
+      props.get("validation.locale.default")
+        .map(Locale::forLanguageTag)
+        .ifPresent(locales::add);
 
-          props
-              .get("validation.locale.default")
-              .map(Locale::forLanguageTag)
-              .ifPresent(locales::add);
+      props.get("validation.locale.addedLocales").stream()
+        .flatMap(s -> Arrays.stream(s.split(",")))
+        .map(Locale::forLanguageTag)
+        .forEach(locales::add);
 
-          props.get("validation.locale.addedLocales").stream()
-              .flatMap(s -> Arrays.stream(s.split(",")))
-              .map(Locale::forLanguageTag)
-              .forEach(locales::add);
-
-          final var beanValidator = new BeanValidator(locales);
-
-          builder.addPostConstruct(beanValidator::setValidator);
-
-          return beanValidator;
-        });
+      final var beanValidator = new BeanValidator(locales);
+      builder.addPostConstruct(beanValidator::setValidator);
+      return beanValidator;
+    });
   }
 }
