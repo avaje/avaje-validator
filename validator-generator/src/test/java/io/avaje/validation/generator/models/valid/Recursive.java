46216eaac1959219b0cd25ae0ac598292ed57b5c
Josiah Noel <32279667+SentryMan@users.noreply.github.com>
2024-08-30 16:34:22 -0400

    Update Recursive.java

--- validator-generator/src/test/java/io/avaje/validation/generator/models/valid/Recursive.java
+++ validator-generator/src/test/java/io/avaje/validation/generator/models/valid/Recursive.java
@@ -1,6 +1,13 @@
 package io.avaje.validation.generator.models.valid;
 
+import java.util.List;
+
+import io.avaje.validation.constraints.NotNull;
 import io.avaje.validation.constraints.Valid;
 
 @Valid
-public record Recursive( String name, @Valid Recursive child) {}
+public record Recursive(
+    @NotNull String name,
+    @Valid @NotNull Recursive child,
+    @Valid Recursive[] array,
+    @Valid List<Recursive> list) {}
