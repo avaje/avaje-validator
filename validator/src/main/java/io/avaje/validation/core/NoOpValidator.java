0ea1a02e4bf36a3ae6ea51f40bde8cfe72c1b97c
Josiah Noel <32279667+SentryMan@users.noreply.github.com>
2024-09-22 13:38:21 -0400

    fix primitive noop

---
+++ validator/src/main/java/io/avaje/validation/core/NoOpValidator.java
@@ -0,0 +1,68 @@
+package io.avaje.validation.core;
+
+import io.avaje.validation.adapter.ValidationAdapter;
+import io.avaje.validation.adapter.ValidationRequest;
+
+class NoOpValidator implements ValidationAdapter, ValidationAdapter.Primitive {
+
+  public static final NoOpValidator INSTANCE = new NoOpValidator();
+
+  @Override
+  public boolean validate(Object value, ValidationRequest req, String propertyName) {
+
+    return true;
+  }
+
+  @Override
+  public Primitive primitive() {
+    return this;
+  }
+
+  @Override
+  public boolean validate(boolean value, ValidationRequest req, String propertyName) {
+
+    return true;
+  }
+
+  @Override
+  public boolean validate(byte value, ValidationRequest req, String propertyName) {
+
+    return true;
+  }
+
+  @Override
+  public boolean validate(char value, ValidationRequest req, String propertyName) {
+
+    return true;
+  }
+
+  @Override
+  public boolean validate(double value, ValidationRequest req, String propertyName) {
+
+    return true;
+  }
+
+  @Override
+  public boolean validate(float value, ValidationRequest req, String propertyName) {
+
+    return true;
+  }
+
+  @Override
+  public boolean validate(int value, ValidationRequest req, String propertyName) {
+
+    return true;
+  }
+
+  @Override
+  public boolean validate(long value, ValidationRequest req, String propertyName) {
+
+    return true;
+  }
+
+  @Override
+  public boolean validate(short value, ValidationRequest req, String propertyName) {
+
+    return true;
+  }
+}
