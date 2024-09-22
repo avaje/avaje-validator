c6e9ccae2fabebb343a2f0579c6de0fc7363b4e8
Rob Bygrave <robin.bygrave@gmail.com>
2024-09-23 07:40:14 +1200

    Make NoOpValidator not public and final

--- validator/src/main/java/io/avaje/validation/core/NoOpValidator.java
+++ validator/src/main/java/io/avaje/validation/core/NoOpValidator.java
@@ -3,13 +3,12 @@ package io.avaje.validation.core;
 import io.avaje.validation.adapter.ValidationAdapter;
 import io.avaje.validation.adapter.ValidationRequest;
 
-public class NoOpValidator implements ValidationAdapter, ValidationAdapter.Primitive {
+final class NoOpValidator implements ValidationAdapter, ValidationAdapter.Primitive {
 
-  public static final NoOpValidator INSTANCE = new NoOpValidator();
+  static final NoOpValidator INSTANCE = new NoOpValidator();
 
   @Override
   public boolean validate(Object value, ValidationRequest req, String propertyName) {
-
     return true;
   }
 
@@ -20,49 +19,41 @@ public class NoOpValidator implements ValidationAdapter, ValidationAdapter.Primi
 
   @Override
   public boolean validate(boolean value, ValidationRequest req, String propertyName) {
-
     return true;
   }
 
   @Override
   public boolean validate(byte value, ValidationRequest req, String propertyName) {
-
     return true;
   }
 
   @Override
   public boolean validate(char value, ValidationRequest req, String propertyName) {
-
     return true;
   }
 
   @Override
   public boolean validate(double value, ValidationRequest req, String propertyName) {
-
     return true;
   }
 
   @Override
   public boolean validate(float value, ValidationRequest req, String propertyName) {
-
     return true;
   }
 
   @Override
   public boolean validate(int value, ValidationRequest req, String propertyName) {
-
     return true;
   }
 
   @Override
   public boolean validate(long value, ValidationRequest req, String propertyName) {
-
     return true;
   }
 
   @Override
   public boolean validate(short value, ValidationRequest req, String propertyName) {
-
     return true;
   }
 }
