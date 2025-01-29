07ac6dbb29b4b7b04d161adfa60b16e42a62e612
Josiah Noel <32279667+SentryMan@users.noreply.github.com>
2025-01-29 01:42:43 -0500

    Revert "nullable can cancel further validation"
    This reverts commit 455f7aaaaaf5060e1f58fb1d3dbcd62d8ef0219a.

--- validator/src/main/java/io/avaje/validation/core/adapters/BasicAdapters.java
+++ validator/src/main/java/io/avaje/validation/core/adapters/BasicAdapters.java
@@ -233,28 +233,18 @@ public final class BasicAdapters {
     }
   }
 
-  private static final class NullableAdapter implements ValidationAdapter<Object> {
+  private static final class NullableAdapter extends AbstractConstraintAdapter<Object> {
 
     private final boolean shouldBeNull;
-    private final ValidationContext.Message message;
-    private final Set<Class<?>> groups;
 
     NullableAdapter(AdapterCreateRequest request, boolean shouldBeNull) {
+      super(request);
       this.shouldBeNull = shouldBeNull;
-      this.groups = request.groups();
-      this.message = request.message();
     }
 
     @Override
-    public boolean validate(Object value, ValidationRequest req, String propertyName) {
-      if (!checkGroups(groups, req)) {
-        return true;
-      }
-      if ((value == null) != shouldBeNull) {
-        req.addViolation(message, propertyName);
-        return false;
-      }
-      return true;
+    public boolean isValid(Object value) {
+      return (value == null) == shouldBeNull;
     }
   }
 
