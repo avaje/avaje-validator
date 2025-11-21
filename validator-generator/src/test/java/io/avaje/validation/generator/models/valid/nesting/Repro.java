c47a36fe7a3f88252fa3d9cdbf35c6a92a79c3e0
Josiah Noel <32279667+SentryMan@users.noreply.github.com>
2025-11-21 10:32:13 -0500

    Nesting Compile Error Test (#331)
    * Nesting Compile Error Test
    
    * Update Repro.java

---
+++ validator-generator/src/test/java/io/avaje/validation/generator/models/valid/nesting/Repro.java
@@ -0,0 +1,12 @@
+package io.avaje.validation.generator.models.valid.nesting;
+
+import io.avaje.validation.constraints.NotNull;
+import io.avaje.validation.constraints.Valid;
+
+public class Repro {
+  @Valid
+  public static class Top {
+    @NotNull public Nested nested;
+    public static record Nested(String prop) {}
+  }
+}
