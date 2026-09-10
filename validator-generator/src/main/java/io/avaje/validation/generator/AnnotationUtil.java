1314c4c4bac208ec5f64ae9d7d87506c8321e38a
Josiah Noel <32279667+SentryMan@users.noreply.github.com>
2026-09-10 11:56:58 -0400

    validate the payload attribute type

--- validator-generator/src/main/java/io/avaje/validation/generator/AnnotationUtil.java
+++ validator-generator/src/main/java/io/avaje/validation/generator/AnnotationUtil.java
@@ -19,6 +19,8 @@ import javax.lang.model.element.Element;
 import javax.lang.model.element.ExecutableElement;
 import javax.lang.model.element.Name;
 import javax.lang.model.element.VariableElement;
+import javax.lang.model.type.ArrayType;
+import javax.lang.model.type.TypeKind;
 import javax.lang.model.type.TypeMirror;
 import javax.lang.model.util.ElementFilter;
 
@@ -321,6 +323,9 @@ final class AnnotationUtil {
     String writeAttributes() {
       validate();
       for (final ExecutableElement member : ElementFilter.methodsIn(element.getEnclosedElements())) {
+        if ("payload".contentEquals(member.getSimpleName())) {
+          validatePayloadType(member);
+        }
         final AnnotationValue value = annotationMirror.getElementValues().get(member);
         final AnnotationValue defaultValue = member.getDefaultValue();
         if (value == null && defaultValue == null) {
@@ -331,6 +336,22 @@ final class AnnotationUtil {
       writeTypeAttribute();
       sb.append(")");
       return sb.toString();
+    }
+
+    private static void validatePayloadType(ExecutableElement member) {
+      final var returnType = member.getReturnType();
+      final var valid =
+          returnType.getKind() == TypeKind.ARRAY
+              && ((ArrayType) returnType)
+                  .getComponentType()
+                  .toString()
+                  .startsWith("java.lang.Class");
+      if (!valid) {
+        logError(
+            member,
+            "payload() must be declared as Class<? extends Payload>[] but is %s",
+            returnType);
+      }
     }
 
     protected void writeTypeAttribute() {
